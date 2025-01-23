7bc56b701ef19fcfba62767e33f7880ff5755ea2
TinaDitte <tina.buster@gmail.com>
2025-01-23 10:24:02 +0100

    Initial Commit

---
+++ Task Management/Task_ManagementApp.swift
@@ -0,0 +1,32 @@
+//
+//  Task_ManagementApp.swift
+//  Task Management
+//
+//  Created by Tina Thomsen on 23/01/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Task_ManagementApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
